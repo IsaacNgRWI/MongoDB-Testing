e09c89aaf5c1e86a76e4edc382b0a55b92dde84f
Isaac <isaacngrwi@gmail.com>
2025-02-11 10:34:46 +0000

    Created a collection in the database and populated it with an entity Added function to print different information about the entity and list of collections in the database.

--- venv/main.py
+++ venv/main.py
@@ -4,6 +4,7 @@ import password
 
 uri = 'mongodb+srv://' + password.mongodb_username + ':' + password.mongodb_password + '@cluster0.2o89z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 # print(uri)
+# feels like an oversight how you could just print the uri and see the password
 
 # creates a new client and connects to the server
 client = pymongo.MongoClient(uri, server_api=ServerApi('1'))
@@ -12,5 +13,20 @@ client = pymongo.MongoClient(uri, server_api=ServerApi('1'))
 try:
     client.admin.command('ping')
     print('MongoDB database successfully connected.')
+    print('-----------------------------------------------')
 except Exception as e:
     print(e)
+
+# sets up the database and the collections(customers) ie. entity
+database = client['test_database']
+collection = database['customers']
+
+# gives the first item in the collection and creates the collection in the database
+customer_dict = {'name': 'Arthur', 'address': 'lemoyne'}
+test1 = collection.insert_one(customer_dict)
+
+print('test1:', test1)
+print('test1.id:', test1.inserted_id)
+
+# prints the names of the different collections in the database
+print('list of collections in the database:', database.list_collection_names())
