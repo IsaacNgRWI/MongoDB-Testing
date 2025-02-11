fca83801d367924506b27c996be85322fb8f8f88
Isaac <isaacngrwi@gmail.com>
2025-02-11 10:46:40 +0000

    Added multiple documents at the same time and documents with specific id

--- venv/main.py
+++ venv/main.py
@@ -19,14 +19,43 @@ except Exception as e:
 
 # sets up the database and the collections(customers) ie. entity
 database = client['test_database']
-collection = database['customers']
+collection_customers = database['customers']
 
 # gives the first item in the collection and creates the collection in the database
-customer_dict = {'name': 'Arthur', 'address': 'lemoyne'}
-test1 = collection.insert_one(customer_dict)
+customer_dict = {'name': 'Arthur', 'address': 'lemoyne'}  # a record (document)
+test1 = collection_customers.insert_one(customer_dict)
 
 print('test1:', test1)
 print('test1.id:', test1.inserted_id)
 
 # prints the names of the different collections in the database
 print('list of collections in the database:', database.list_collection_names())
+
+# inserting multiple documents into a collection
+customer_dict_list = [
+  { "name": "Amy", "address": "Apple st 652"},
+  { "name": "Hannah", "address": "Mountain 21"},
+  { "name": "Michael", "address": "Valley 345"},
+  { "name": "Sandy", "address": "Ocean blvd 2"},
+  { "name": "Betty", "address": "Green Grass 1"},
+  { "name": "Richard", "address": "Sky st 331"},
+  { "name": "Susan", "address": "One way 98"},
+  { "name": "Vicky", "address": "Yellow Garden 2"},
+  { "name": "Ben", "address": "Park Lane 38"},
+  { "name": "William", "address": "Central st 954"},
+  { "name": "Chuck", "address": "Main Road 989"},
+  { "name": "Viola", "address": "Sideway 1633"}
+]  # lifted directly from w3 school
+
+# inserts the whole list of documents into the collection
+test2 = collection_customers.insert_many(customer_dict_list)
+print('test2:', test2)
+
+# inserting documents with specified ids
+specific_id_customer = {'_id': 1909, 'name': 'John', 'address': 'Mexico'}
+try:
+    test3 = collection_customers.insert_one(specific_id_customer)
+    print('test3.id:', test3.inserted_id)
+except Exception as e:
+    print('ERROR OCCURRED:', e)
+# it seems that you cannot have two documents with the same id and running the program again will result in an error
